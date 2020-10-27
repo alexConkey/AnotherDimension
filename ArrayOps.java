3804cf5bbd00c7f42171119b2080eb69487185a3
alexConkey <aconkey20@stuy.edu>
2020-10-27 03:23:23 -0400

    largest done

--- ArrayOps.java
+++ ArrayOps.java
@@ -6,4 +6,13 @@ public class ArrayOps{
     }
     return sum;
   }
+  public static int largest(int[] arr){
+    int greatest = arr[0];
+    for(int i = 0; i < arr.length; i++){
+      if(arr[i]>greatest){
+        greatest = arr[i];
+      }
+    }
+    return greatest;
+  }
 }
--- Tester.java
+++ Tester.java
@@ -1,6 +1,7 @@
 public class Tester{
   public static void main(String[] args) {
-    int[] testArr = {1, 2};
+    int[] testArr = {1, 2, 5, 8 ,20};
     System.out.println(ArrayOps.sum(testArr));
+    System.out.println(ArrayOps.largest(testArr));
   }
 }
